76d5f8ab437fed136d796bb3c62a742757888748
Bruno <brunobrito728@gmail.com>
2022-11-09 17:44:19 -0300

    Requisito 03 completo

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">Settings</h1>
+      </div>
+    );
+  }
+}
+
+export default Settings;
